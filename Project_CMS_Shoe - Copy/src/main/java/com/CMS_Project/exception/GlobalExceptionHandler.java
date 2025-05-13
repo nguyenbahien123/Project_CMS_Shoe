d69fb173ebac2edb13f003b29ac541de8be99a39
nguyenbahien123 <nguyenbahien170604@gmail.com>
2025-05-13 16:38:22 +0700

    delete project coppy

--- Project_CMS_Shoe - Copy/src/main/java/com/CMS_Project/exception/GlobalExceptionHandler.java
+++
@@ -1,9 +0,0 @@
-package com.CMS_Project.exception;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-
-@ControllerAdvice
-@Slf4j
-public class GlobalExceptionHandler {
-}
